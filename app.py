a071de4fff67cca0c0c01b6739610ece5bd55d83
Ben Esse <benjamin.esse@manchester.ac.uk>
2022-01-28 09:45:08 +0000

    Remove extra pages

--- app.py
+++ app.py
@@ -40,50 +40,5 @@ def index():
                            station_name=station_name)
 
 
-@app.route('/<station_name>/')
-def station_landing(station_name):
-    """Generate the station landing page."""
-    # Set date to today
-    date_to_plot = str(datetime(2022, 1, 27).date())
-    # str(datetime.now().date())
-
-    # Get the available dates
-    dates = [d for d in os.listdir(fpath) if os.path.isdir(f'{fpath}/{d}')]
-    dates.reverse()
-
-    # Generate the plots
-    SO2graphJSON = generate_map(fpath, date_to_plot, 'SO2', 'SO2 SCD',
-                                'viridis', [0, 2e18])
-    INTgraphJSON = generate_map(fpath, date_to_plot, 'int_av', 'Intensity',
-                                'magma')
-
-    return render_template('station_landing.html',
-                           dates=dates,
-                           data_date=date_to_plot,
-                           SO2graphJSON=SO2graphJSON,
-                           INTgraphJSON=INTgraphJSON,
-                           station_name=station_name)
-
-
-@app.route('/<station_name>/<date_to_plot>/')
-def station_page(station_name, date_to_plot):
-    """Generate the station page."""
-    # Get the available dates
-    dates = [d for d in os.listdir(fpath) if os.path.isdir(f'{fpath}/{d}')]
-    dates.reverse()
-
-    # Generate the plots
-    SO2graphJSON = generate_map(fpath, date_to_plot, station_name, 'SO2',
-                                'SO2 SCD', 'viridis', [0, 2e18])
-    INTgraphJSON = generate_map(fpath, date_to_plot, station_name, 'int_av',
-                                'Intensity', 'magma')
-
-    return render_template('station_past_data.html',
-                           date_to_plot=date_to_plot,
-                           SO2graphJSON=SO2graphJSON,
-                           INTgraphJSON=INTgraphJSON,
-                           station_name=station_name)
-
-
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
