c9d05563247c65f80f263efd17abc983dcaaeaf2
Ben Esse <benjamin.esse@manchester.ac.uk>
2022-02-14 11:07:10 +0000

    Trim status date

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def index(datestr):
     try:
         with open(f"{config['DataPath']}/Station/status.txt", 'r') as r:
             status_time, status_text = r.readline().split(' - ')
+            status_time = datetime.strftime(status_time, "%Y-%m-%d %H:%M:%S.%f"
+                                            ).strptime("%Y-%m-%d %H:%M:%S")
     except FileNotFoundError:
         status_text, status_time = 'Unknown', '???'
 
