88395ece972e8735bac553f6953e37385cdf9aea
Ben Esse <benjamin.esse@manchester.ac.uk>
2022-02-14 11:27:05 +0000

    Bug fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('/station', datestr='today'))
+    return redirect(url_for('station', datestr='today'))
 
 
 @app.route('/<datestr>')
